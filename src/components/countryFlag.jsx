9bb3a5d5d0931d4db7b5b1aeb6974fae270af8e1
kavery-pm <shiny.kavery@gmail.com>
2023-03-23 09:31:46 +0100

    added the search filter and the filter by region filter design

--- src/components/countryFlag.jsx
+++ src/components/countryFlag.jsx
@@ -1,35 +1,65 @@
 import { useEffect, useState } from "react";
 import "../../src/style.css";
-import { DATA } from "../models/countryData";
+
 const CountryFlag = () => {
-    const [countryDetails, setcountryDetails] = useState([]);
-useEffect(() => {
- fetchCountryData();
-}, [])
-const fetchCountryData = async()=>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then((res) => res.json())
-    .then(
-      (result) => {
-        console.log(result)
-       setcountryDetails(result)
-       
-      },
-      (error) => {
-        console.log(error)
-      }
-    );
-}
+  const [countryDetails, setcountryDetails] = useState([]);
+  useEffect(() => {
+    fetchCountryData();
+  }, []);
+  const fetchCountryData = async () => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setcountryDetails(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
   return (
     <>
-        <div className="wrapper">
+      <div className="wrapper">
+      <div className="search-wrapper">
+          <label htmlFor="search-form">
+            <input
+              type="search"
+              name="search-form"
+              id="search-form"
+              className="search-input"
+              placeholder="Search for..."
+              
+              
+            />
+            <span className="sr-only">Search Country</span>
+          </label>
+          <div className="select">
+            <select
+           
+              className="custom-select"
+              aria-label="Filter Countries By Countries"
+            >
+              <option value="All">Filter By Region</option>
+              <option value="Africa">Africa</option>
+              <option value="Americas">America</option>
+              <option value="Asia">Asia</option>
+              <option value="Europe">Europe</option>
+              <option value="Oceania">Oceania</option>
+            </select>
+            <span className="focus"></span>
+          </div>
+        </div>
         <ul className="card-grid">
-        {countryDetails.map(countryItem=>(
-            <li >
-              <article className="card" >
+          {countryDetails.map((countryItem) => (
+            <li>
+              <article className="card">
                 <div className="card-image">
-                   
-                  <img src={countryItem.flags.png} alt={countryItem.name.common}/>
+                  <img
+                    src={countryItem.flags.png}
+                    alt={countryItem.name.common}
+                  />
                 </div>
                 <div className="card-content">
                   <h2 className="card-name">{countryItem.name.common}</h2>
@@ -47,11 +77,9 @@ const fetchCountryData = async()=>{
                 </div>
               </article>
             </li>
-        ))}
-            
-        
+          ))}
         </ul>
-        </div>
+      </div>
     </>
   );
 };
