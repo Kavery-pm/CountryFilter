601b1b58f7799dfe1d6c790ed30157e623e1b026
kavery-pm <shiny.kavery@gmail.com>
2023-03-23 08:35:56 +0100

    added the country details dynamically

--- src/components/countryFlag.jsx
+++ src/components/countryFlag.jsx
@@ -1,10 +1,11 @@
 import "../../src/style.css";
+import { DATA } from "../models/countryData";
 const CountryFlag = () => {
   return (
     <>
         <div className="wrapper">
         <ul className="card-grid">
-        
+        {DATA.map(countryItem=>(
             <li >
               <article className="card" >
                 <div className="card-image">
@@ -12,21 +13,23 @@ const CountryFlag = () => {
                   <img src='https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1200px-Flag_of_Germany.svg.png' />
                 </div>
                 <div className="card-content">
-                  <h2 className="card-name">Germany</h2>
+                  <h2 className="card-name">{countryItem.name}</h2>
                   <ol className="card-list">
                     <li>
                       population: <span>10000</span>
                     </li>
                     <li>
-                      Region: <span>Europe</span>
+                      Region: <span>{countryItem.region}</span>
                     </li>
                     <li>
-                      Capital: <span>Berlin</span>
+                      Capital: <span>{countryItem.capital}</span>
                     </li>
                   </ol>
                 </div>
               </article>
             </li>
+        ))}
+            
         
         </ul>
         </div>
